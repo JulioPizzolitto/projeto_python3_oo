a367f1010cdd73a033c1c385309d7249894de2fd
Julio Pizzolitto <jcmagalhaes@yahoo.com>
2020-06-18 12:32:06 -0300

    Herdando propriedades de list

---
+++ modelo_03.py
@@ -0,0 +1,57 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def dar_like(self):
+        self._likes += 1
+
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - Likes: {self.likes}')
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.duracao} min - Likes: {self.likes}')
+
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self.temporadas} temporadas - Likes: {self.likes}')
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
+vingadores.imprime()
+
+atlanta = Serie('atlanta', 2018, 2)
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.imprime()
+print("\n")
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    programa.imprime()
--- modelo_04.py
+++ modelo_04.py
@@ -41,40 +41,17 @@ class Serie(Programa):
         return f'{self.nome} - {self.ano} - {self.temporadas} temporadas - Likes: {self.likes}'
 
 
-class Playlist(list):
-    def __init__(self, nome, programas):
-        self.nome = nome
-        super().__init__(programas)
-
-
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
+print(vingadores)
+
 atlanta = Serie('atlanta', 2018, 2)
-tmep = Filme('Todo mundo em panico', 2018, 100)
-demolidor = Serie('Demolidor', 2016, 2)
-
-vingadores.dar_like()
-vingadores.dar_like()
-tmep.dar_like()
-tmep.dar_like()
-tmep.dar_like()
-demolidor.dar_like()
-demolidor.dar_like()
-demolidor.dar_like()
-demolidor.dar_like()
-demolidor.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
-#print(atlanta)
-#print("\n")
+print(atlanta)
+print("\n")
 
-filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+filmes_e_series = [vingadores, atlanta]
 
-playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
-
-print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
-
-for programa in playlist_fim_de_semana:
+for programa in filmes_e_series:
     print(programa)
-
-print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
-
